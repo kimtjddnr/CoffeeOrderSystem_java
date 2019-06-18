8623b17ea649ca3c8b2623e423af61c2b87b2b52
kimtjddnr <38129927+kimtjddnr@users.noreply.github.com>
2019-06-18 21:24:40 +0900

    change korean to english

---
+++ Coffee.java
@@ -0,0 +1,39 @@
+
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class Coffee {
+	int count=0;
+	String show="";
+	
+	public Coffee() {
+		//set Frame
+		JFrame frame=new JFrame("coffee order system");
+		frame.setBounds(0,0,625,1000);
+		frame.setBackground(Color.black);
+		
+		Font font = new Font(Font.SANS_SERIF, Font.PLAIN, 18);
+        Font font1 = new Font(Font.MONOSPACED, Font.BOLD, 22);
+        
+        
+
+        
+
+	}
+	
+	
+}
